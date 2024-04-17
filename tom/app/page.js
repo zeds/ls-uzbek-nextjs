1a60d10ae6f601fbb2d6945bcd20f6e680954640
zeds <tom.zed39@gmail.com>
2024-04-18 03:33:09 +0700

    header

--- tom/app/page.js
+++ tom/app/page.js
@@ -1,21 +1,35 @@
 export default function Home() {
 	return (
-		<main className="w-full h-screen bg-red-200">
-			<header className="my-grid bg-green-400">
-				<div className="flex items-center justify-center">
-					<img className="h-[20px]" src="logo.svg"></img>
+		<main className="w-full h-screen bg-white">
+			<header className="my-grid">
+				<div className="flex items-center justify-center ml-4">
+					<div className="w-[40px] h-[40px] flex items-center justify-center shrink-0">
+						<img className="h-[24px]" src="burger.svg"></img>
+					</div>
+					<div className="flex h-full py-[18px] pl-[14px] pr-[16px] shrink-0">
+						<img className="h-[20px]" src="logo.svg"></img>
+					</div>
 				</div>
-				<div className="bg-blue-200 flex items-center justify-center px-4">
-					<input className="h-[40px] w-full max-w-[536px]"></input>
+				<div className="items-center justify-center px-1 ml-[80px] hidden sm:flex">
+					<input
+						placeholder="検索"
+						className="pl-4 h-[40px] w-full max-w-[536px] rounded-l-full outline outline-gray-300"
+					></input>
 
 					{/* scope */}
-					<button className="w-[64px] h-[40px] bg-gray-200">
-						<img src="scope.svg"></img>
+					<button className="shrink-0 w-[64px] h-[40px] bg-gray-200 relative rounded-r-full border-2 border-solid outline outline-gray-300">
+						<img
+							className="w-[24px] absolute right-4 bottom-1.5"
+							src="scope.svg"
+						></img>
 					</button>
 
 					{/* mic */}
-					<button className="w-[40px] h-[40px] bg-gray-500 ml-[12px]">
-						<img src="mic.svg"></img>
+					<button className="ml-[12px] w-[40px] h-[40px] rounded-full bg-gray-300 flex items-center justify-center aspect-square">
+						<img
+							className="flex shrink-0 rounded-full w-[24px]"
+							src="mic.svg"
+						></img>
 					</button>
 				</div>
 				<ul className="flex items-center justify-center gap-2">
