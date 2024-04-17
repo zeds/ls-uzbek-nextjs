3ddda3b035ea4ee41c51c25302bf1c4da2e1f925
Shoxrux71 <amirjonovdamin@gmail.com>
2024-04-17 19:40:52 +0500

    npm

--- shoxrux/app/page.js
+++ shoxrux/app/page.js
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <div className="w-[400px] h-[100px] bg-blue-600"></div>
-      <div className="w-[400px] h-[100px] bg-blue-600"></div>
-    </main>
-  );
+  return <main className="w-full h-screen bg-red-200"></main>;
 }
-// npm
