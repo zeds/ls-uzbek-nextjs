7ec04e52846e989f7d26638d7b2588e61a5581e8
zeds <tom.zed39@gmail.com>
2024-04-17 23:04:19 +0700

    header

--- tom/app/page.js
+++ tom/app/page.js
@@ -1,14 +1,27 @@
 export default function Home() {
 	return (
 		<main className="w-full h-screen bg-red-200">
-			{/* 169px 1fr 156px */}
-			<div className="my-grid bg-blue-400">
-				<div className="flex justify-center items-center">
+			<header className="my-grid bg-green-400">
+				<div className="flex items-center justify-center">
 					<img className="h-[20px]" src="logo.svg"></img>
 				</div>
-				<div className="bg-green-200">いいい</div>
-				<div>ううう</div>
-			</div>
+				<div className="bg-blue-200 flex items-center justify-center px-4">
+					<input className="h-[40px] w-full max-w-[536px]"></input>
+					<button className="w-[64px] h-[40px] bg-gray-200"></button>
+					<button className="w-[40px] h-[40px] bg-gray-500 ml-[12px]"></button>
+				</div>
+				<ul className="flex items-center justify-center gap-2">
+					<li className="p-2">
+						<img className="h-[24px]" src="video.svg"></img>
+					</li>
+					<li className="p-2">
+						<img className="h-[24px]" src="bell.svg"></img>
+					</li>
+					<li className="py-[1px] px-[6px]">
+						<img className="h-[32px] rounded-full" src="tom.png"></img>
+					</li>
+				</ul>
+			</header>
 		</main>
 	);
 }
