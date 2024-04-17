f0f75bb306e58642042fb8e1a5bfb150913a76e0
sarviee <210239s@gmail.com>
2024-04-17 19:22:11 +0500

    change

--- sarviee/app/page.js
+++ sarviee/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-[200px] h-[100px] bg-red-600"></div>
+      <div className="w-[200px] h-[100px] bg-red-600">aaa</div>
     </main>
   );
 }
