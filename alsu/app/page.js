c1a3b6bf0b0d92c94678a6de4ffc3efa60d9966b
alsu <alsuyildirim2004@gmail.com>
2024-04-19 19:47:12 +0500

    change title

--- alsu/app/page.js
+++ alsu/app/page.js
@@ -4,8 +4,13 @@ export default function Home() {
   return (
     <main className="w-full h-screen bg-red-200">
       <header className="my-grid bg-green-400">
-        <div className="flex items-center justify-center bg-yellow-200">
-          <img className="h-[20px]" src="logo.svg"></img>
+        <div className="flex items-center justify-center ml-4">
+          <div className="bg-red-200 w-10 h-10 p-2 shrink-0">
+            <img src="burger.svg"></img>
+          </div>
+          <div className="w-[127px] h-[56px] bg-blue-300 py-[18px] pl-[16px] pr-[14px] shrink-0">
+            <img src="logo.svg"></img>
+          </div>
         </div>
 
         <div className="bg-blue-200 flex items-center justify-center px-4 ">
