217b0fe4cf3fe67a26d26122c8fa71b98d2bf24b
alsu <alsuyildirim2004@gmail.com>
2024-04-19 18:28:54 +0500

    change title

--- alsu/app/page.js
+++ alsu/app/page.js
@@ -2,8 +2,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-[200px] h-[100px] bg-red-600">ffff</div>
+    <main className="w-full h-screen bg-red-200">
+      <header className="my-grid bg-green-400">
+        <div className="flex items-center justify-center bg-yellow-200">
+          <img className="h-[20px]" src="logo.svg"></img>
+        </div>
+
+        <div className="bg-blue-200 flex items-center justify-center px-4 ">
+          <input className="h-[40px] w-full max-w-[536px] rounded-l-full"></input>
+          {/*scope*/}
+          <button className="w-[64px] h-[40px] bg-gray-200 flex items-center justify-center rounded-r-full ">
+            <img src="scope.svg"></img>
+          </button>
+          {/*mic*/}
+          <button className=" ml-[12px] rounded full flex items-center justify-center aspect-ratio">
+            <img
+              className="flex rounded-full w-[40px] bg-gray-200 "
+              src="mic.svg"
+            ></img>
+          </button>
+        </div>
+        {/*video*/}
+        <ul className="flex items-center justify-center gap-2">
+          <li className="p-2">
+            <img className="h-[24px]" src="video.svg"></img>
+          </li>
+          {/*bell*/}
+          <li className="p-2">
+            <img className="h-[24px]" src="bell.svg"></img>
+          </li>
+          <li className="p-2">
+            <img className="h-[32px] rounded-full" src="tom.png"></img>
+          </li>
+        </ul>
+      </header>
     </main>
   );
 }
