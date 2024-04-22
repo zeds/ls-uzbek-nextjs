e31b042ed4830efcc4dedf05972dc486af49f6a2
zeds <tom.zed39@gmail.com>
2024-04-22 19:43:41 +0700

    profile

--- tom/app/page.js
+++ tom/app/page.js
@@ -63,7 +63,7 @@ export default function Home() {
 					{/* Mr Tom */}
           <li className="flex justify-center w-[60px] h-[34px] px-[6px] py-[1px]">
             <img
-              className="w-[32px] h=[32px] rounded-full"
+              className="w-[32px] h-[32px] rounded-full"
               src="tom.png"
               alt=""
             ></img>
