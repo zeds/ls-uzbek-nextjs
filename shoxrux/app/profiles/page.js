ac3f2e431d97cf8bb75c792fa43f478ff2e04e22
Shoxrux71 <amirjonovdamin@gmail.com>
2024-04-22 20:31:58 +0500

    client and server

--- shoxrux/app/profiles/page.js
+++ shoxrux/app/profiles/page.js
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className="flex justify-around items-center mt-20">
       <div>
-        <h1>プロフィール</h1> <img id='img' className="w-[320px] rounded-2xl" src="fff.jpg"></img>
+        <h1 className="text-center font-medium">プロフィール</h1> <img id='img' className="w-[620px] rounded-2xl object-cover" src="fff.jpg"></img>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full pl-[50px] pr-[50px] mt-5">btn</button>
       </div>
     </div>
