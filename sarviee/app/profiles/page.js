649c48434e1305065a63fa71244a8485af074b38
sarviee <210239s@gmail.com>
2024-04-22 20:59:38 +0500

    aiueo

--- sarviee/app/profiles/page.js
+++ sarviee/app/profiles/page.js
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
+import { useCallback, useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
 
 const page = () => {
-  return <div>Profile</div>;
+  const supabase = createClient();
+  const [dataSource, setDataSource] = useState([]);
+
+  const getProfile = useCallback(async () => {
+    try {
+      //   setLoading(true)
+
+      const { data, error, status } = await supabase
+        .from("profiles2")
+        .select("*");
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        console.log("data = ", data);
+        setDataSource(data);
+      }
+    } catch (error) {
+      alert("Error loading user data!");
+    } finally {
+      //   setLoading(false)
+    }
+  }, []);
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  return (
+    <>
+      <div>プロフィール</div>
+      <div>
+        {dataSource.map((item) => (
+          <div>{item.username}</div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default page;
