472049a71615712d4ca4225cad525bc69cb95759
richi911 <shakhriyor.life@gmail.com>
2024-04-28 15:31:22 +0500

    richi911

--- richi911/app/page.js
+++ richi911/app/page.js
@@ -1,9 +1,29 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-[200px] h-[100px] bg-red-600"></div>
+    <main className="w-full h-screen bg-red-200">
+      <div className="my-grid bg-blue-400">
+        <div className="flex justify-center items-center">
+          <img className="h-[20px]" src="youtube-logo.svg"></img>
+        </div>
+        <div className="bg-green-200 flex justify-center items-center">
+          <input className="w-full max-w-[536px] h-[40px]"></input>
+          <button className="w-[64px] h-[40px] bg-gray-400">1</button>
+          <button className="aspect-square rounded-full ml-[12px] w-[40px] h-[40px] bg-gray-400">
+            1
+          </button>
+        </div>
+        <ul class="list-none flex justify-center items-center gap-2">
+          <li className="p-2">
+            <img className="h-[24px]" src="create.svg"></img>
+          </li>
+          <li className="p-2">
+            <img className="h-[24px]" src="notifications.svg"></img>
+          </li>
+          <li className="p-2">
+            <img className="h-[32px] rounded-full" src="profile.jpg"></img>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
