f4ac00260cdc79fe72413089c6d93e635491666a
mexroj2603 <sayfullayevmexroj27@gmail.com>
2024-05-01 21:22:49 +0500

    01.05

--- mexroj/app/test/page.js
+++ mexroj/app/test/page.js
@@ -5,6 +5,9 @@ const Page = () => {
   const [name, setName] = useState("tom");
   const [age, setAge] = useState(30);
 
+  const [isEdit, setIsEdit] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   const [profiles, setProfiles] = useState([
     { name: "tom", age: 18 },
     { name: "mexroj", age: 20 },
@@ -15,17 +18,24 @@ const Page = () => {
   ]);
 
   const clickAdd = () => {
-    setProfiles([...profiles, { name, age }]);
+    if (isEdit) {
+      let newArr = [...profiles];
+      newArr[currentIndex].name = name;
+      newArr[currentIndex].age = age;
+      setProfiles(newArr);
+    } else {
+      setProfiles([...profiles, { name: name, age: age }]);
+    }
+    setIsEdit(false);
   };
 
   const clickEdit = (index) => {
     let newArr = [...profiles];
     setAge(newArr[index].age);
     setName(newArr[index].name);
-    newArr.splice(index, 1);
-    setProfiles(newArr);
-    // newArr[index].age = 50;
-    // setProfiles(newArr);
+
+    setIsEdit(true);
+    setCurrentIndex(index);
   };
   const clickDelete = (index) => {
     let delteValue = [...profiles];
@@ -35,13 +45,13 @@ const Page = () => {
 
   return (
     <div className="p-3">
-      <div className="w-[300px] bg-pink-300  p-3 gap-3">
+      <div className="w-[300px] bg-pink-300  p-3 gap-3 items-center justify-center">
         <div className="flex gap-3 mb-2 ">
           <div>名前:</div>
           <input
             value={name}
             onChange={(event) => setName(event.target.value)}
-            className="w-[200px] bg-white right-[10px]"
+            className="p-1"
           ></input>
         </div>
         <div className="flex gap-3 ">
@@ -49,23 +59,25 @@ const Page = () => {
           <input
             value={age}
             onChange={(event) => setAge(event.target.value)}
-            className="w-[200px] bg-white right-[10px]"
+            className="p-1"
           ></input>
+        </div>
+        <div className="flex w-full h-[50px] justify-center items-center">
+          <button
+            onClick={() => clickAdd()}
+            className="bg-[#00FF00] rounded-md px-10 py-2 mt-5"
+          >
+            {isEdit ? "更新" : "追加"}
+          </button>
         </div>
       </div>
       <div className="flex my-4">
         <div className="">
           私の名前は{name}です,私は{age}齢です。よろしくお願いします。
         </div>
-        <button
-          onClick={() => clickAdd()}
-          className="bg-[#00FF00] rounded-md px-2 py-1 hover:opacity-70"
-        >
-          追加
-        </button>
       </div>
       {profiles.map((item, index) => (
-        <div key={index} className="flex m-3 items-center bg-gray-400">
+        <div key={index} className="flex m-3 items-center bg-gray-300">
           <div className="w-[120px] ">名前：{item.name}</div>
           <div className="w-[80px]">年齢：{item.age}</div>
           <button
