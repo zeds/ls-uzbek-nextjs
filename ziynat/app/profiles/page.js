1442db04cf9d3cc9adc57d76115cf83ef05dbf58
Ziynat555 <ziynat.zl56@gmail.com>
2024-05-01 17:48:37 +0500

    ziynat

--- ziynat/app/profiles/page.js
+++ ziynat/app/profiles/page.js
@@ -2,9 +2,42 @@
 import { useCallback, useEffect, useState } from "react";
 import { createClient } from "@/utils/supabase/client";
 
-const page = () => {
-  const supabase = createClient();
+const Page = () => {
   const [dataSource, setDataSource] = useState([]);
+
+  const supabase = createClient();
+
+  const channelA = supabase
+    .channel("schema-db-changes")
+    .on(
+      "postgres_changes",
+      {
+        event: "*",
+        schema: "public",
+        // テーブルを指定しない場合、全てのテーブルの変更が通知される。
+        // table: "notifications",
+      },
+      (payload) => {
+        if (payload.table === "profiles") {
+          if (payload.new && "username" in payload.new) {
+            updateProfile(payload.new);
+            // setMessage(payload.new.introduce);
+          }
+        }
+      }
+    )
+    .subscribe();
+
+  const updateProfile = (target) => {
+    // mapで見つけて更新する
+    let newArr = [...dataSource];
+    newArr.map((item, index) => {
+      if (target.id === item.id) {
+        newArr[index] = target;
+        setDataSource(newArr);
+      }
+    });
+  };
 
   const getProfile = useCallback(async () => {
     try {
@@ -12,7 +45,8 @@ const page = () => {
 
       const { data, error, status } = await supabase
         .from("profiles")
-        .select("*");
+        .select("*")
+        .order("id", { ascending: false });
 
       if (error && status !== 406) {
         throw error;
@@ -33,23 +67,61 @@ const page = () => {
     getProfile();
   }, []);
 
-  const onChangeHandler = (event) => {
-    setDataSource([...dataSource, { username: event.target.value }]);
-    //setUserName(event, target, value);
+  const onChangeHandler = (name, index) => {
+    console.log(dataSource[index].username);
+    // dataSourceを直接編集できない
+    // dataSourceのコピーを作って、編集したあと、コピーをdataSourceに上書きする
+    let newArr = [...dataSource];
+    newArr[index].username = name;
+    setDataSource(newArr);
+  };
+
+  const clickUpdate = async (index) => {
+    // alert(JSON.stringify(dataSource[index]))
+
+    // usernameを更新
+    const { error: updateError } = await supabase
+      .from("profiles")
+      .update({
+        username: dataSource[index].username,
+      })
+      .eq("id", dataSource[index].id); // 'cf3466fb-1af1-48ec-9868-73437564da11'
+    // update profiles set username = 'とむかも' where id = 'cf3466fb-1af1-48ec-9868-73437564da11'
+
+    // alert(updateError)  // nullはエラーがない意味
+    if (updateError !== null) {
+      alert("失敗！：" + JSON.stringify(updateError));
+    }
   };
 
   return (
     <>
       <div>プロフィール</div>
+      {JSON.stringify(dataSource)}
       <div className="flex bg-gray-300 gap-1 flex-wrap p-2">
-        {dataSource.map((item) => (
-          <div key={item.id} className="w-[200px} h-[300px] bg-blue-300">
-            <input
-              type="text"
-              name="name"
-              onChange={onChangeHandler}
-              value={item.username}
-            />
+        {dataSource.map((item, index) => (
+          <div
+            key={index}
+            className="w-[200px] h-[300px] bg-blue-300 relative p-2"
+          >
+            <div>
+              <div>Name</div>
+              <input
+                className="p-1 bg-red-200 w-full"
+                type="text"
+                name="username"
+                onChange={(e) => onChangeHandler(e.target.value, index)}
+                value={item.username}
+              />
+            </div>
+            <div className="justify-center flex absolute bottom-1 start-1/3">
+              <button
+                onClick={() => clickUpdate(index)}
+                className="bg-blue-400 text-white px-3 py-1 rounded-md"
+              >
+                更新
+              </button>
+            </div>
           </div>
         ))}
       </div>
@@ -57,4 +129,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
