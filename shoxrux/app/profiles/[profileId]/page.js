2a20d01a100bd146fde0a545f8ec7f66856eb921
Shoxrux71 <amirjonovdamin@gmail.com>
2024-05-03 21:34:52 +0500

    n

---
+++ shoxrux/app/profiles/[profileId]/page.js
@@ -0,0 +1,53 @@
+"use client";
+import { useCallback, useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+
+const page = () => {
+    const [dataSource, setDataSource] = useState([]);
+    const supabase = createClient()
+
+    const getProfile = useCallback(async () => {
+		try {
+			//   setLoading(true)
+
+			const { data, error, status } = await supabase
+				.from("profiles")
+				.select("*")
+				.order("id", { ascending: false });
+
+			if (error && status !== 406) {
+				throw error;
+			}
+
+			if (data) {
+				console.log("data = ", data);
+				setDataSource(data);
+			}
+		} catch (error) {
+			alert("Error loading user data!");
+		} finally {
+			//   setLoading(false)
+		}
+	}, []);
+
+	useEffect(() => {
+		getProfile();
+	}, []);
+
+
+    return (
+        <div className='w-full flex justify-center'>
+            <div className='w-[600px] h-screen bg-red-300 flex justify-center'>
+                <div className='url_icon'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default page
