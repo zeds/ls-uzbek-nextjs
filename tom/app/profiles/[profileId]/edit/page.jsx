d905b0ed042d4e91d0f0b8b9c5a5dc6e06a70a83
zeds <tom.zed39@gmail.com>
2024-05-08 18:05:33 +0700

    delete node_modules

--- tom/app/profiles/[profileId]/edit/page.jsx
+++ tom/app/profiles/[profileId]/edit/page.jsx
@@ -3,22 +3,20 @@ import { useCallback, useEffect, useState } from "react";
 import { createClient } from "@/utils/supabase/client";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 
-const Profile = () => {
+const ProfileEdit = () => {
 	const [dataSource, setDataSource] = useState({});
 	const supabase = createClient();
 	const profileId = usePathname().split("/")[2];
 	const route = useRouter();
 
+	const [userName, setUserName] = useState("");
+	const [email, setEmail] = useState("");
+
 	useEffect(() => {
 		getProfile();
 		console.log("profileId=", profileId);
 	}, []);
-
-	const clickEdit = () => {
-		route.push(`/profiles/${profileId}/edit`);
-	};
 
 	const getProfile = useCallback(async () => {
 		try {
@@ -36,6 +34,8 @@ const Profile = () => {
 
 			if (data) {
 				console.log("data = ", data);
+				setUserName(data.username);
+				setEmail(data.email);
 				setDataSource(data);
 			}
 		} catch (error) {
@@ -45,27 +45,57 @@ const Profile = () => {
 		}
 	}, []);
 
+	const clickSave = async () => {
+		// alert(JSON.stringify(dataSource[index]))
+
+		// usernameを更新
+		const { error: updateError } = await supabase
+			.from("profiles")
+			.update({
+				username: userName,
+				email: email,
+			})
+			.eq("id", dataSource.id); // 'cf3466fb-1af1-48ec-9868-73437564da11'
+
+		// alert(updateError)  // nullはエラーがない意味
+		if (updateError !== null) {
+			alert("失敗！：" + JSON.stringify(updateError));
+		} else {
+			route.push(`/profiles/${profileId}`);
+		}
+	};
+
+	// const clickSave = () => {
+	// 	route.push(`/profiles/${profileId}`);
+	// };
+
 	return (
 		<div className="w-full h-screen bg-blue-300 flex justify-center">
 			{/* 576pxの外枠 */}
-			<div className="w-full max-w-xl  bg-gradient-to-r from-indigo-500">
-				<Link href={`/profiles`} className="underline">
-					プロフィール一覧へ
-				</Link>
+			<div className="w-full max-w-xl bg-pink-300">
+				<h1>編集画面</h1>
 				{/* アバター */}
 				<div className="w-full flex justify-center items-center mt-5 flex-col">
 					<img className="w-[80px]" src={dataSource.avatar_url} alt="" />
-					<label className="w-full px-[100px] h-[30px] mt-10">
-						username: {dataSource.username}
-					</label>
-					<label className="w-full px-[100px] h-[30px]">
-						email: {dataSource.email}
-					</label>
+					<label>username</label>
+					<input
+						className="w-[300px] p-2"
+						type="text"
+						value={userName}
+						onChange={(e) => setUserName(e.target.value)}
+					></input>
+					<label>email</label>
+					<input
+						className="w-[300px] p-2"
+						type="text"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					></input>
 					<button
-						onClick={clickEdit}
+						onClick={clickSave}
 						className="mt-5 p-2 bg-blue-500 text-white rounded-md"
 					>
-						プロフィールを編集
+						保存
 					</button>
 				</div>
 			</div>
@@ -73,4 +103,4 @@ const Profile = () => {
 	);
 };
 
-export default Profile;
+export default ProfileEdit;
