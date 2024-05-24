db099dee6c953fa70852e067c9ab4e2401eb0051
sarviee <210239s@gmail.com>
2024-05-24 21:17:50 +0500

    YT

--- sarviee/components/Article.jsx
+++ sarviee/components/Article.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-const Article = ({ title, avatar }) => {
+const Article = ({ title, avatar, user_name, stats, img_url }) => {
   return (
     <div className="w-[300px] h-[200px] bg-blue-300">
+      <img src={img_url} />
       <img className="w-[60px]" src={avatar} alt="image"></img>
       <div>{title}</div>
+      <div>{user_name}</div>
+      <div>{stats}</div>
     </div>
   );
 };
