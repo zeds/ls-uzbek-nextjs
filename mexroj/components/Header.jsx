4a0a483647b7b68467060c2d71ad9be0f0c9e283
mexroj2603 <sayfullayevmexroj27@gmail.com>
2024-05-25 08:56:14 +0500

    25.05

--- mexroj/components/Header.jsx
+++ mexroj/components/Header.jsx
@@ -12,18 +12,18 @@ const Header = () => {
       {showModal ? (
         <FlagModal setShowModal={setShowModal} setFlag={setFlag} />
       ) : null}
-      <div className="fixed z-10 flex h-[56px] w-full bg-gray-500 justify-between items-center">
-        <div className="flex items-center justify-center w-[169px] bg-blue-500">
-          <div className="bg-red-200 w-10 h-10 p-2 shrink-0">
+      <div className="fixed z-10 flex h-[56px] w-full justify-between items-center bg-white">
+        <div className="flex items-center justify-center w-[169px] ">
+          <div className="w-10 h-10 p-2 shrink-0">
             <img src="burger.svg" alt=""></img>
           </div>
           <Link href={"/"}>
-            <div className="w-[127px] h-[56px] bg-blue-300 py-[18px] pl-[16px] pr-[14px] shrink-0">
+            <div className="w-[127px] h-[56px] py-[18px] pl-[16px] pr-[14px] shrink-0">
               <img src="logo.svg" alt=""></img>
             </div>
           </Link>
         </div>
-        <div className="ml-[90px] w-full max-w-[732px] h-[40px] bg-orange-200 hidden sm:flex justify-end">
+        <div className="ml-[90px] w-full max-w-[732px] h-[40px] hidden sm:flex justify-end">
           {/* search */}
           <input
             placeholder="検索"
@@ -48,14 +48,14 @@ const Header = () => {
             ></img>
           </button>
         </div>
-        <ul className="flex items-center justify-end w-[225px] h-[40px] bg-red-500 shrink-0">
+        <ul className="flex items-center justify-end w-[225px] h-[40px]  shrink-0">
           {/* search */}
           <li className="flex shrink-0 sm:hidden w-[40px] h-[40px] p-2">
-            <img className="bg-green-200" src="scope.svg" alt=""></img>
+            <img className="" src="scope.svg" alt=""></img>
           </li>
           {/* mic */}
           <li className="flex shrink-0 sm:hidden w-[40px] h-[40px] p-2">
-            <img className="bg-green-500" src="mic.svg" alt=""></img>
+            <img className="" src="mic.svg" alt=""></img>
           </li>
 
           {/* flag 国旗 */}
@@ -72,11 +72,11 @@ const Header = () => {
 
           {/* video */}
           <li className="flex items-center shrink-0 w-[40px] h-[40px] p-2">
-            <img className="bg-green-500" src="video.svg" alt=""></img>
+            <img className="" src="video.svg" alt=""></img>
           </li>
           {/*bell*/}
           <li className="flex items-center shrink-0 w-[40px] h-[40px] p-2">
-            <img className="bg-green-500" src="bell.svg" alt=""></img>
+            <img className="" src="bell.svg" alt=""></img>
           </li>
           {/* Mr Tom */}
           <Link href={"/profiles"}>
