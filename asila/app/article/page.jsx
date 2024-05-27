020dbbd495b6df08d860e788b859d07c2759a6a0
AsilaMamanova <asilamamanova272@gmail.com>
2024-05-27 19:06:22 +0500

    article

---
+++ asila/app/article/page.jsx
@@ -0,0 +1,210 @@
+import React from "react";
+import "../globals.css";
+import Image from "next/image";
+
+const page = () => {
+  return (
+    <div class="container">
+      <div className="frame">
+        <Image
+          width={0}
+          height={0}
+          sizes="100vw"
+          src="/azizaka.webp"
+          alt="Aziz"
+          style={{ width: "100%", height: "auto", borderRadius: "12px" }}
+        />
+
+        <div className="decs">
+          <Image
+            width={0}
+            height={0}
+            sizes="100vw"
+            src="/summer.webp"
+            alt="Aziz"
+            style={{
+              width: "36px",
+              height: "36px",
+              borderRadius: "18px",
+              marginTop: "3px",
+            }}
+          />
+          <div>
+            <div className="title">
+              Bugun birinchi marta vazifa qildingiz | Iqtibos podcast
+            </div>
+            <div className="author">Aziz Rahimov</div>
+            <div className="statistics">1,67 миллиона подписчиков</div>
+          </div>
+        </div>
+      </div>
+      <div className="frame">
+        <Image
+          width={0}
+          height={0}
+          sizes="100vw"
+          src="/spring.webp"
+          alt="Aziz"
+          style={{ width: "100%", height: "auto", borderRadius: "12px" }}
+        />
+
+        <div className="decs">
+          <Image
+            width={0}
+            height={0}
+            sizes="100vw"
+            src="/spring.webp"
+            alt="Aziz"
+            style={{
+              width: "36px",
+              height: "36px",
+              borderRadius: "18px",
+              marginTop: "3px",
+            }}
+          />
+          <div>
+            <div className="title">
+              Bugun birinchi marta vazifa qildingiz | Iqtibos podcast
+            </div>
+            <div className="author">Aziz Rahimov</div>
+            <div className="statistics">1,67 миллиона подписчиков</div>
+          </div>
+        </div>
+      </div>
+      <div className="frame">
+        <Image
+          width={0}
+          height={0}
+          sizes="100vw"
+          src="/autumn.webp"
+          alt="Aziz"
+          style={{ width: "100%", height: "auto", borderRadius: "12px" }}
+        />
+
+        <div className="decs">
+          <Image
+            width={0}
+            height={0}
+            sizes="100vw"
+            src="/gar.webp"
+            alt="Aziz"
+            style={{
+              width: "36px",
+              height: "36px",
+              borderRadius: "18px",
+              marginTop: "3px",
+            }}
+          />
+          <div>
+            <div className="title">
+              Bugun birinchi marta vazifa qildingiz | Iqtibos podcast
+            </div>
+            <div className="author">Aziz Rahimov</div>
+            <div className="statistics">1,67 миллиона подписчиков</div>
+          </div>
+        </div>
+      </div>
+      <div className="frame">
+        <Image
+          width={0}
+          height={0}
+          sizes="100vw"
+          src="/gar.webp"
+          alt="Aziz"
+          style={{ width: "100%", height: "auto", borderRadius: "12px" }}
+        />
+
+        <div className="decs">
+          <Image
+            width={0}
+            height={0}
+            sizes="100vw"
+            src="/azizaka.webp"
+            alt="Aziz"
+            style={{
+              width: "36px",
+              height: "36px",
+              borderRadius: "18px",
+              marginTop: "3px",
+            }}
+          />
+          <div>
+            <div className="title">
+              Bugun birinchi marta vazifa qildingiz | Iqtibos podcast
+            </div>
+            <div className="author">Aziz Rahimov</div>
+            <div className="statistics">1,67 миллиона подписчиков</div>
+          </div>
+        </div>
+      </div>
+      <div className="frame">
+        <Image
+          width={0}
+          height={0}
+          sizes="100vw"
+          src="/spring.webp"
+          alt="Aziz"
+          style={{ width: "100%", height: "auto", borderRadius: "12px" }}
+        />
+
+        <div className="decs">
+          <Image
+            width={0}
+            height={0}
+            sizes="100vw"
+            src="/azizaka.webp"
+            alt="Aziz"
+            style={{
+              width: "36px",
+              height: "36px",
+              borderRadius: "18px",
+              marginTop: "3px",
+            }}
+          />
+          <div>
+            <div className="title">
+              Bugun birinchi marta vazifa qildingiz | Iqtibos podcast
+            </div>
+            <div className="author">Aziz Rahimov</div>
+            <div className="statistics">440K views 2 months ago</div>
+          </div>
+        </div>
+      </div>
+      <div className="frame">
+        <Image
+          width={0}
+          height={0}
+          sizes="100vw"
+          src="/gar.webp"
+          alt="Aziz"
+          style={{ width: "100%", height: "auto", borderRadius: "12px" }}
+        />
+
+        <div className="decs">
+          <Image
+            width={0}
+            height={0}
+            sizes="100vw"
+            src="/gar.webp"
+            alt="Aziz"
+            style={{
+              width: "36px",
+              height: "36px",
+              borderRadius: "18px",
+              marginTop: "3px",
+            }}
+          />
+          <div>
+            <div className="title">
+              Bugun birinchi marta vazifa qildingiz | Iqtibos podcast
+            </div>
+            <div className="author">Aziz Rahimov</div>
+            <div className="statistics">1,67 миллиона подписчиков</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
