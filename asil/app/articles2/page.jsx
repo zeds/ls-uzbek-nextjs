8aadd386bdcc23dc9dd5d1d14b7abb15e0f9552f
Asil KURASH <apple@Asil-KURASH.local>
2024-05-29 20:11:28 +0900

    24/05/29

---
+++ asil/app/articles2/page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const page = () => {
+	return (
+		<div className="pt-14">
+			<div className="w-[380px] border border-black flex">
+				<div className="w-[36px] h-[36px] flex-shrink-0">
+					<img src="raichel-avatar.jpeg" alt="rachel" />
+				</div>
+				<div>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+					exercitationem, et voluptate, nesciunt qui dolor reiciendis rem
+					est assumenda eius delectus ad corrupti vitae. Saepe velit fuga
+					expedita perspiciatis eos.
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default page;
