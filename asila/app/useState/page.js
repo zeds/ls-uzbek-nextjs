5537dfda34d56eeddc37b100cd150279b8650760
AsilaMamanova <asilamamanova272@gmail.com>
2024-05-31 20:03:16 +0500

    article

--- asila/app/useState/page.js
+++ asila/app/useState/page.js
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
 
-const page = () => {
-  return (
-    <div className="pt-[56px]">page</div>
-  )
-}
+const Page = () => {
+	const [value, setValue] = useState("useStateの練習をするよ");
 
-export default page
+
+	const clickSearch = () => {
+		alert("押されたばい: " + value);
+	};
+
+	return (
+		<div className="pt-[56px] bg-red-300 h-screen flex justify-center items-center">
+			<div className="flex">
+				<input type="text" onChange={(e) => setValue(e.target.value)}></input>
+				<button onClick={clickSearch} className="bg-blue-300 p-3">
+					検索
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Page;
