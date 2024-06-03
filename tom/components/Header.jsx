fd1a3c6f6c14ba016627f653140f243474791a67
zeds <tom.zed39@gmail.com>
2024-06-03 18:47:01 +0700

    useStore

--- tom/components/Header.jsx
+++ tom/components/Header.jsx
@@ -2,29 +2,41 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import FlagModal from "./FlagModal";
+import { useCounterStore } from "@/store";
 
 const Header = () => {
 	const [flag, setFlag] = useState("jp"); // jp us au
 	const [showModal, setShowModal] = useState(false); // true/false
+	const count = useCounterStore((state) => state.count);
+	const increment = useCounterStore((state) => state.increment);
+	const decrement = useCounterStore((state) => state.decrement);
 
 	return (
 		<>
 			{showModal ? (
 				<FlagModal setShowModal={setShowModal} setFlag={setFlag} />
 			) : null}
-			//fixed
-			<div className=" z-10 flex h-[56px] w-full bg-gray-500 justify-between items-center">
-				<div className="flex items-center justify-center w-[169px] bg-blue-500">
-					<div className="bg-red-200 w-10 h-10 p-2 shrink-0">
+			<div className="fixed z-10 flex h-[56px] w-full justify-between items-center bg-white">
+				<div className="flex items-center justify-center w-[169px] ">
+					<div className="w-10 h-10 p-2 shrink-0">
 						<img src="burger.svg" alt=""></img>
 					</div>
 					<Link href={"/"}>
-						<div className="w-[127px] h-[56px] bg-blue-300 py-[18px] pl-[16px] pr-[14px] shrink-0">
+						<div className="w-[127px] h-[56px] py-[18px] pl-[16px] pr-[14px] shrink-0">
 							<img src="logo.svg" alt=""></img>
 						</div>
 					</Link>
+					<div className="flex items-center">
+						<button onClick={increment} className="p-1 border m-1">
+							+
+						</button>
+						{count}
+						<button onClick={decrement} className="p-1 border m-1">
+							-
+						</button>
+					</div>
 				</div>
-				<div className="ml-[90px] w-full max-w-[732px] h-[40px] bg-orange-200 hidden sm:flex justify-end">
+				<div className="ml-[90px] w-full max-w-[732px] h-[40px] hidden sm:flex justify-end">
 					{/* search */}
 					<input
 						placeholder="検索"
@@ -49,14 +61,14 @@ const Header = () => {
 						></img>
 					</button>
 				</div>
-				<ul className="flex items-center justify-end w-[225px] h-[40px] bg-red-500 shrink-0">
+				<ul className="flex items-center justify-end w-[225px] h-[40px]  shrink-0">
 					{/* search */}
 					<li className="flex shrink-0 sm:hidden w-[40px] h-[40px] p-2">
-						<img className="bg-green-200" src="scope.svg" alt=""></img>
+						<img className="" src="scope.svg" alt=""></img>
 					</li>
 					{/* mic */}
 					<li className="flex shrink-0 sm:hidden w-[40px] h-[40px] p-2">
-						<img className="bg-green-500" src="mic.svg" alt=""></img>
+						<img className="" src="mic.svg" alt=""></img>
 					</li>
 
 					{/* flag 国旗 */}
@@ -73,18 +85,18 @@ const Header = () => {
 
 					{/* video */}
 					<li className="flex items-center shrink-0 w-[40px] h-[40px] p-2">
-						<img className="bg-green-500" src="video.svg" alt=""></img>
+						<img className="" src="video.svg" alt=""></img>
 					</li>
 					{/*bell*/}
 					<li className="flex items-center shrink-0 w-[40px] h-[40px] p-2">
-						<img className="bg-green-500" src="bell.svg" alt=""></img>
+						<img className="" src="bell.svg" alt=""></img>
 					</li>
 					{/* Mr Tom */}
 					<Link href={"/profiles"}>
 						<li className="flex justify-center w-[60px] h-[34px] px-[6px] py-[1px]">
 							<img
 								className="w-[32px] h-[32px] rounded-full"
-								src="tom.jpeg"
+								src="me.jpg"
 								alt=""
 							></img>
 						</li>
