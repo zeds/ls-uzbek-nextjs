fe9586bfe0e872795f58318e85d9515c9c1567eb
saloh2 <salohiddingulboyev7@gmail.com>
2024-06-03 19:05:10 +0500

    改善

--- salohi/app/articles/page.jsx
+++ salohi/app/articles/page.jsx
@@ -1,110 +1,138 @@
-"use client";
-import React, { useCallback, useState, useEffect } from "react";
-import "../globals.css";
-import Image from "next/image";
-import { createClient } from "@/utils/supabase/client";
+	"use client";
+	import React, { useState, useEffect } from "react";
+	import "../globals.css";
 
-function Page() {
-	const [searchKeyword, setSearchKeyword] = useState("");
-	const [dataSource, setDataSource] = useState([]);
-	const supabase = createClient();
-
-	const getArticles = useCallback(async (keyword) => {
-		try {
-			console.log("searchKeyword=", keyword); // piano
-			keyword = "%" + keyword + "%"; // %piano%
-
-			const { data, error, status } = await supabase
-				.from("articles")
-				.select("*")
-				// .textSearch("title", "Tokyo")
-				// .ilike("title", keyword) // %piano%
-				.order("id", { ascending: false });
-
-			if (error && status !== 406) {
-				throw error;
-			}
-
-			if (data) {
-				console.log("data = ", data);
-				setDataSource(data);
-			}
-		} catch (error) {
-			alert("Error loading user data!");
-		} finally {
-			//   setLoading(false)
-		}
-	}, []);
-
-	useEffect(() => {
-		getArticles(searchKeyword);
-	}, [getArticles]);
-
-	useEffect(() => {
-		console.log("us=", searchKeyword);
-		getArticles(searchKeyword);
-	}, [searchKeyword]);
-
-	const searchArticles = () => {
-		alert("検索します");
-		getArticles(searchKeyword);
-	};
-
+	const Page = () => {
 	return (
-		<div>
-			<div className="flex pt-[56px] ml-2">
-				<input className="bg-red-200"></input>
-				<button>検索</button>
+		<div className="grid-frame">
+		<div className="article">
+			<img className="photo" src="vid.webp" al="vid" />
+			<div className="desc">
+			<img className="img" src="pic1.jpg" al="pic1" />
+			<div>
+				<div className="title">
+				Relaxing Rainy Drive in Iceland | Beautiful car drive, Lagoon,
+				Rain Sounds for Sleep ASMR
+				</div>
+				<div className="user">Nomadic Ambience</div>
+				<div className="stats">1.25M Subscribers</div>
 			</div>
-			<div className="container">
-				{dataSource.map((item, index) => (
-					<div key={index} className="w-full">
-						<Image
-							width={0}
-							height={0}
-							sizes="100vw"
-							src={item.img_url}
-							alt="rasm"
-							style={{
-								width: "100%",
-								height: "auto",
-								borderRadius: "12px",
-							}}
-						/>
-
-						<div className="flex gap-[10px] mt-3 mr-3">
-							<Image
-								width={0}
-								height={0}
-								sizes="100vw"
-								src={item.avatar_url}
-								alt="rasm"
-								style={{
-									width: "36px",
-									height: "36px",
-									borderRadius: "18px",
-									marginTop: "3px",
-								}}
-							/>
-							<div>
-								<div className="line-clamp-2 leading-[22px] text-[rgba(15,15,15,1)]">
-									{item.title}
-									{/* Free BGM "I'll be sleepy after a snack" 2 hours ver -
-								Kawaii Afternoon Break [NoCopyrightMusic] */}
-								</div>
-								<div className="line-clamp-1 text-sm font-normal text-[rgba(15,15,15,1)]">
-									{item.user_name}
-								</div>
-								<div className="line-clamp-1 text-sm font-normal text-[rgba(96,96,96,1)]">
-									{item.stats}
-								</div>
-							</div>
-						</div>
-					</div>
-				))}
 			</div>
 		</div>
+		<div className="article">
+			<img className="photo" src="vid.webp" al="vid" />
+			<div className="desc">
+			<img className="img" src="pic1.jpg" al="pic1" />
+			<div>
+				<div className="title">
+				Relaxing Rainy Drive in Iceland | Beautiful car drive, Lagoon,
+				Rain Sounds for Sleep ASMR
+				</div>
+				<div className="user">Nomadic Ambience</div>
+				<div className="stats">1.25M Subscribers</div>
+			</div>
+			</div>
+		</div>
+		<div className="article">
+			<img className="photo" src="vid.webp" al="vid" />
+			<div className="desc">
+			<img className="img" src="pic1.jpg" al="pic1" />
+			<div>
+				<div className="title">
+				Relaxing Rainy Drive in Iceland | Beautiful car drive, Lagoon,
+				Rain Sounds for Sleep ASMR
+				</div>
+				<div className="user">Nomadic Ambience</div>
+				<div className="stats">1.25M Subscribers</div>
+			</div>
+			</div>
+		</div>
+		<div className="article">
+			<img className="photo" src="vid.webp" al="vid" />
+			<div className="desc">
+			<img className="img" src="pic1.jpg" al="pic1" />
+			<div>
+				<div className="title">
+				Relaxing Rainy Drive in Iceland | Beautiful car drive, Lagoon,
+				Rain Sounds for Sleep ASMR
+				</div>
+				<div className="user">Nomadic Ambience</div>
+				<div className="stats">1.25M Subscribers</div>
+			</div>
+			</div>
+		</div>
+		<div className="article">
+			<img className="photo" src="vid.webp" al="vid" />
+			<div className="desc">
+			<img className="img" src="pic1.jpg" al="pic1" />
+			<div>
+				<div className="title">
+				Relaxing Rainy Drive in Iceland | Beautiful car drive, Lagoon,
+				Rain Sounds for Sleep ASMR
+				</div>
+				<div className="user">Nomadic Ambience</div>
+				<div className="stats">1.25M Subscribers</div>
+			</div>
+			</div>
+		</div>
+		<div className="article">
+			<img className="photo" src="vid.webp" al="vid" />
+			<div className="desc">
+			<img className="img" src="pic1.jpg" al="pic1" />
+			<div>
+				<div className="title">
+				Relaxing Rainy Drive in Iceland | Beautiful car drive, Lagoon,
+				Rain Sounds for Sleep ASMR
+				</div>
+				<div className="user">Nomadic Ambience</div>
+				<div className="stats">1.25M Subscribers</div>
+			</div>
+			</div>
+		</div>
+		<div className="article">
+			<img className="photo" src="vid.webp" al="vid" />
+			<div className="desc">
+			<img className="img" src="pic1.jpg" al="pic1" />
+			<div>
+				<div className="title">
+				Relaxing Rainy Drive in Iceland | Beautiful car drive, Lagoon,
+				Rain Sounds for Sleep ASMR
+				</div>
+				<div className="user">Nomadic Ambience</div>
+				<div className="stats">1.25M Subscribers</div>
+			</div>
+			</div>
+		</div>
+		<div className="article">
+			<img className="photo" src="vid.webp" al="vid" />
+			<div className="desc">
+			<img className="img" src="pic1.jpg" al="pic1" />
+			<div>
+				<div className="title">
+				Relaxing Rainy Drive in Iceland | Beautiful car drive, Lagoon,
+				Rain Sounds for Sleep ASMR
+				</div>
+				<div className="user">Nomadic Ambience</div>
+				<div className="stats">1.25M Subscribers</div>
+			</div>
+			</div>
+		</div>
+		<div className="article">
+			<img className="photo" src="vid.webp" al="vid" />
+			<div className="desc">
+			<img className="img" src="pic1.jpg" al="pic1" />
+			<div>
+				<div className="title">
+				Relaxing Rainy Drive in Iceland | Beautiful car drive, Lagoon,
+				Rain Sounds for Sleep ASMR
+				</div>
+				<div className="user">Nomadic Ambience</div>
+				<div className="stats">1.25M Subscribers</div>
+			</div>
+			</div>
+		</div>
+		</div>
 	);
-}
+	};
 
-export default Page;
+	export default Page;
