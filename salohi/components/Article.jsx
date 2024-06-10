8a23a773fc26f24349bc8dd4e63a483e6802c16d
saloh2 <salohiddingulboyev7@gmail.com>
2024-06-10 19:17:04 +0500

    改善

--- salohi/components/Article.jsx
+++ salohi/components/Article.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 const Article = ({ title, avatar, user_name, stats, img_url }) => {
 	return (
-		<div className="w-[300px] h-[200px] bg-blue-300">
-			<img src={img_url} />
-			<img className="w-[60px]" src={avatar} alt="image"></img>
-			<div>{title}</div>
-			<div>{user_name}</div>
-			<div>{stats}</div>
+		<div className="article-container  grid-cols-4">
+			<div className=" bg-blue-300 w-[300px]">
+				<img className="w-[90%]m-auto" src={img_url} />
+				<img className="w-[60px] rounded-full" src={avatar} alt="image"></img>
+				<div>{title}</div>
+				<div>{user_name}</div>
+				<div>{stats}</div>
+			</div>
 		</div>
 	);
 };
+
+
 
 export default Article;
