08e6e2f2727c76cfd96fa74fd0e88ed7ab2d3dbb
Ziynat555 <ziynat.zl56@gmail.com>
2024-06-10 10:09:45 +0500

    ziynat

--- ziynat/app/useState/page.jsx
+++ ziynat/app/useState/page.jsx
@@ -1,33 +1,34 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { useCounterStore } from "@/store";
 import React, { useEffect, useState } from "react";
 
 let update = true;
 
 const Page = () => {
-	const text = useCounterStore((state) => state.text);
+  const text = useCounterStore((state) => state.text);
 
-	const [value, setValue] = useState("useStateの練習をするよ");
+  const [value, setValue] = useState("useStateの練習をするよ");
 
-	const clickSearch = () => {
-		alert("入力された値：" + value);
-	};
+  const clickSearch = () => {
+    alert("入力された値：" + value);
+  };
 
-	return (
-		<div className="pt-[56px] bg-red-300 h-screen flex justify-center items-center">
-			{text}
-			<div className="flex">
-				<input
-					className="p-2"
-					type="text"
-					onChange={(e) => setValue(e.target.value)}
-				></input>
-				<button onClick={clickSearch} className="bg-blue-300 p-3">
-					検索
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="pt-[56px] bg-red-300 h-screen flex justify-center items-center">
+      {text}
+      <div className="flex">
+        <input
+          className="p-2"
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        ></input>
+        <Button onClick={clickSearch} className="bg-blue-300 p-3">
+          検索
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
