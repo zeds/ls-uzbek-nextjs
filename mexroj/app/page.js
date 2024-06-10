cea341b9b54856575b89509c8ac9ded2caed1e14
mexroj2603 <sayfullayevmexroj27@gmail.com>
2024-06-10 18:48:33 +0500

    kk

--- mexroj/app/page.js
+++ mexroj/app/page.js
@@ -36,18 +36,53 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-wrap bg-red-300 w-full pt-[56px] gap-2">
-      {dataSource.map((item, index) => (
-        <div key={index}>
-          <Article
-            title={item.title}
-            avatar={item.avatar_url}
-            user_name={item.user_name}
-            stats={item.stats}
-            img_url={item.img_url}
-          />
-        </div>
-      ))}
+    <div className="">
+      <div className="w-full pt-16 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-3 bg-white  ">
+        {dataSource.map((item, index) => (
+          <div key={index} className="w-full">
+            <Image
+              width={0}
+              height={0}
+              sizes="100vw"
+              src={item.img_url}
+              alt="rasm"
+              style={{
+                width: "100%",
+                height: "auto",
+                borderRadius: "12px",
+              }}
+            />
+            <div className="flex gap-[10px] mt-3 ">
+              <Image
+                width={0}
+                height={0}
+                sizes="100vw"
+                src={item.avatar_url}
+                alt="rasm"
+                style={{
+                  width: "36px",
+                  height: "36px",
+                  borderRadius: "18px",
+                  marginTop: "3px",
+                }}
+              />
+              <div>
+                <div className="line-clamp-2 leading-[22px] text-[rgba(15,15,15,1)]">
+                  {item.title}
+                  {/* Free BGM "I'll be sleepy after a snack" 2 hours ver -
+              Kawaii Afternoon Break [NoCopyrightMusic] */}
+                </div>
+                <div className="line-clamp-1 text-sm font-normal text-[rgba(15,15,15,1)]">
+                  {item.user_name}
+                </div>
+                <div className="line-clamp-1 text-sm font-normal text-[rgba(96,96,96,1)]">
+                  {item.stats}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
