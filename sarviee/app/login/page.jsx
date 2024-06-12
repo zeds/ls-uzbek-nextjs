71a36108b518500fbbfa90da2b12af2dc86bfb14
sarviee <210239s@gmail.com>
2024-06-12 20:12:18 +0500

    new

--- sarviee/app/login/page.jsx
+++ sarviee/app/login/page.jsx
@@ -7,16 +7,22 @@ const page = () => {
   return (
     <div className="w-full bg-blue-300 h-screen flex justify-center items-center">
       <div className="flex flex-col w-[400px] p-4 bg-yellow-300">
-        <div className="my-2">ユーザー名</div>
+        <div className="my-2 font-bold">ユーザー名</div>
         <Input type="email" placeholder="ユーザー名" />
-        <div className="my-2">パスワード</div>
+        <div className="my-2 font-bold">パスワード</div>
         <Input type="password" placeholder="パスワード" />
 
-        <Link href="#" class=" my-2 font-medium text-blue-600 hover:underline">
+        <Link
+          href="#"
+          class=" font-bold my-2 font-medium text-blue-600 hover:underline"
+        >
           パスワードを忘れた
         </Link>
 
-        <Button className="mt-4 bg-black text-white" variant="outline">
+        <Button
+          className="font-bold mt-4 bg-black text-white"
+          variant="outline"
+        >
           ログイン
         </Button>
       </div>
