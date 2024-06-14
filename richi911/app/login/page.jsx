15e55bcbcb33199781b400182c5ab068ccef0065
richi911 <shakhriyor.life@gmail.com>
2024-06-14 17:14:56 +0500

    richi911

---
+++ richi911/app/login/page.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-screen bg-gray-500">
+      <div className="flex flex-col w-[400px] bg-gray-200 p-4 rounded-md">
+        <div className="my-2 font-semibold">Username:</div>
+        <div className="flex">
+          <div className="w-10 h-10 mr-0.5 flex items-center justify-center">
+            <img src="/person.svg" alt="person" />
+          </div>
+          <Input type="email" placeholder="Email" />
+        </div>
+        <div className="my-2 font-semibold">Password:</div>
+        <div className="flex">
+          <div className="w-10 h-10 mr-0.5 flex items-center justify-center">
+            <img src="/password.svg" alt="password" />
+          </div>
+          <Input type="password" placeholder="Password" />
+        </div>
+        <Link
+          href="/forget-password"
+          className="text-blue-500 underline pt-2 text-sm"
+        >
+          Forgot password?
+        </Link>
+        <Button className="mt-5">Login</Button>
+      </div>
+    </div>
+  );
+};
+
+export default page;
