67cdf9b3aa88ecb445e64ca3525baf3de390d4c0
qahnus02 <qutbiddinovsamandar@gmail.com>
2024-06-19 16:20:48 +0500

    new

--- samandar/app/page.js
+++ samandar/app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
 	}, []);
 
 	useEffect(() => {
-		getArticles("text");
+		getArticles(text);
 	}, [text]);
 
 	return (
--- samandar/store/index.js
+++ samandar/store/index.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 export const useCounterStore = create((set) => ({
 	isLogin: true,
-	text: "あいうえお",
+	text: "",
 	changeText: (value) => {
 		set({ text: value });
 	},
