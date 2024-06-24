6189767db905aae80e7a01318d25fdbc7e496ba3
Shoxrux71 <amirjonovdamin@gmail.com>
2024-06-24 20:56:40 +0500

    M

--- shoxrux/youtub-tailwind/app/layout.tsx
+++ shoxrux/youtub-tailwind/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+import Article from "@/components/Article";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="w-full h-dvh bg-sky-300">
+          <Header />
+          <Navbar />
+          <Article />
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
