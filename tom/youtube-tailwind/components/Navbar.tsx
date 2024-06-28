f6dbca7801eb6f9f9dd247869b61919f02c8e43d
zeds <tom.zed39@gmail.com>
2024-06-28 21:28:32 +0700

    nav

--- tom/youtube-tailwind/components/Navbar.tsx
+++ tom/youtube-tailwind/components/Navbar.tsx
@@ -11,18 +11,15 @@ export default function Navbar() {
 	];
 
 	return (
-		<div className="fixed w-[72px] md:w-[72px] lg:w-[216px]  top-[56px] mt-3 left-0 bottom-0 border-4 border-orange-400 z-30">
+		<nav>
 			<ul>
 				{items.map((item, index) => (
-					<li
-						key={index}
-						className="flex flex-col md:flex-col lg:flex-row items-center p-2 text-[10px] lg:text-base line-clamp-1 md:line-clamp-1"
-					>
+					<li key={index}>
 						<img src={item.img} alt="home" />
 						<span className="title">{item.title}</span>
 					</li>
 				))}
 			</ul>
-		</div>
+		</nav>
 	);
 }
