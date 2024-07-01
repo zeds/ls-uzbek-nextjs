477216a64f74e3a9bf2ff39952c451ab03c57fbe
Ziynat555 <ziynat.zl56@gmail.com>
2024-07-01 16:47:50 +0500

    ziynat

--- ziynat/youtube-tailwind/components/Navbar.tsx
+++ ziynat/youtube-tailwind/components/Navbar.tsx
@@ -1,9 +1,37 @@
+import Image from "next/image";
 import React from "react";
 
+// CSJ
+
 export default function Navbar() {
+  const items = [
+    { title: "ホーム", img: "./images/menu_home.svg" },
+    { title: "ショート", img: "./images/menu_short.svg" },
+    { title: "登録チャンネル", img: "./images/.svg" },
+    { title: "YouTube Music", img: "./images/menu_music.svg" },
+    { title: "", img: "" },
+    { title: "チャンネル", img: "./images/.svg" },
+    { title: "履歴", img: "./images/.svg" },
+    { title: "再生リスト", img: "./images/.svg" },
+    { title: "再生した動画", img: "./images/.svg" },
+  ];
+
   return (
-    <div className="absolute w-[216px] top-0 left-0 bottom-0 border-4 border-orange-400">
-      Navbar
-    </div>
+    <nav>
+      <ul>
+        {items.map((item, index) => (
+          <>
+            {item.title === "" ? (
+              <div className="separator" />
+            ) : (
+              <li key={index}>
+                <img src={item.img} alt="home" />
+                <span className="title">{item.title}</span>
+              </li>
+            )}
+          </>
+        ))}
+      </ul>
+    </nav>
   );
 }
