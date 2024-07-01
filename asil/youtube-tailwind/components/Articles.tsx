1837b0f3c83e70c5a2b763f1de4b359dfc04229a
Asil KURASH <apple@Asil-KURASH.local>
2024-07-01 20:14:16 +0900

    01/07/24

--- asil/youtube-tailwind/components/Articles.tsx
+++ asil/youtube-tailwind/components/Articles.tsx
@@ -1,81 +1,32 @@
-import Image from "next/image";
 import React from "react";
 
-export default function Articles() {
-	const articles = [
-		{
-			title: "Partially Examined Life #335: Aristotle on Fundamental Explanations (Part Two)",
-			img: "/images/01.jpg",
-			authorImg: "/images/001.jpg",
-			author: "The Partially Examined Life",
-			statistics: "10人回視聴・1時間前",
-		},
-		{
-			title: "Partially Examined Life #335: Aristotle on Fundamental Explanations (Part Two)",
-			img: "/images/01.jpg",
-			authorImg: "/images/001.jpg",
-			author: "The Partially Examined Life",
-			statistics: "10人回視聴・1時間前",
-		},
-		{
-			title: "Partially Examined Life #335: Aristotle on Fundamental Explanations (Part Two)",
-			img: "/images/01.jpg",
-			authorImg: "/images/001.jpg",
-			author: "The Partially Examined Life",
-			statistics: "10人回視聴・1時間前",
-		},
-		{
-			title: "Partially Examined Life #335: Aristotle on Fundamental Explanations (Part Two)",
-			img: "/images/01.jpg",
-			authorImg: "/images/001.jpg",
-			author: "The Partially Examined Life",
-			statistics: "10人回視聴・1時間前",
-		},
-		{
-			title: "Partially Examined Life #335: Aristotle on Fundamental Explanations (Part Two)",
-			img: "/images/01.jpg",
-			authorImg: "/images/001.jpg",
-			author: "The Partially Examined Life",
-			statistics: "10人回視聴・1時間前",
-		},
-		{
-			title: "Partially Examined Life #335: Aristotle on Fundamental Explanations (Part Two)",
-			img: "/images/01.jpg",
-			authorImg: "/images/001.jpg",
-			author: "The Partially Examined Life",
-			statistics: "10人回視聴・1時間前",
-		},
-	];
+export default function Article() {
+  const articles = [
+    { title: "Partially Examined Life #335: Aristotle on Fundamental Explanations (Part Two)", img: "/images/01.jpg", author: "The Partially Examined Life", author_img: "/images/001.jpg", statistics: "10人回視聴・1時間前" },
+    { title: "When an Illegal Act Takes a Life", img: "/images/02.jpg", author: "Ben Shapiro", author_img: "/images/002.jpg", statistics: "668万人回視聴・2週間前" },
+    { title: "Varieties of Successorship (Set Theory)", img: "/images/03.jpg", author: "Carneades.org", author_img: "/images/003.jpg", statistics: "127回視聴・4時間前" },
+    { title: "Madame Web - YMS", img: "/images/04.jpg", author: "YMS", author_img: "/images/004.jpg", statistics: "20万回視聴・8時間前" },
+    { title: "My Rewrite Of 'That Funny Feeling' By Bo Burnham", img: "/images/05.jpg", author: "Amala Ekpunobi", author_img: "/images/005.jpg", statistics: "1.9万回視聴・13時間前" },
+  ];
 
-	return (
-		<section className="pl-[240px] pr-[20px] pt-[68px] h-full grid grid-cols-3 gap-[10px]">
-			{articles.map((article, index) => (
-				<article key={index} className="w-full aspect-[2/1.8]">
-					<Image
-						width={100}
-						height={100}
-						src={article.img}
-						alt="Top Article"
-						className="w-full rounded-[8px]"
-					/>
-					<div className="flex gap-[10px] mt-[10px]">
-						<Image
-							width={100}
-							height={100}
-							src={article.authorImg}
-							alt="Author"
-							className="w-[36px] h-[36px] rounded-full"
-						/>
-						<div className="w-full">
-							<div className="line-clamp-1 text-[16px] font-medium text-[#0f0f0f]">
-								{article.title}
-							</div>
-							<div className="text-[14px]">{article.author}</div>
-							<div className="text-[14px]">{article.statistics}</div>
-						</div>
-					</div>
-				</article>
-			))}
-		</section>
-	);
+  return (
+    <div className="article-container">
+      {articles.map((item, index) => (
+        <div key={index} className="article">
+          <div className="thumbnail">
+            <img src={item.img} alt="thumbnail" />
+            <div className="time">09:28</div>
+          </div>
+          <div className="content">
+            <img src={item.author_img} alt="author avatar" className="author-avatar" />
+            <div className="info">
+              <h3 className="title">{item.title}</h3>
+              <p className="author">{item.author}</p>
+              <p className="statistics">{item.statistics}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
