7df01b67cdc477c3645c0e606480974ff72238d7
zeds <tom.zed39@gmail.com>
2024-07-01 19:30:53 +0700

    header

--- tom/youtube-tailwind/components/Header.tsx
+++ tom/youtube-tailwind/components/Header.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function Header() {
 	return (
-		<div className="fixed top-0 w-full h-[56px] border-4 border-solid border-green-400 bg-green-700 z-10">
-			Header
+		<div className="fixed top-0 w-full h-[56px] bg-red-300">
+			<div className="logo">
+				<img src="/images/youtube-logo.svg" alt="youtube" />
+			</div>
+			<div>Search</div>
+			<div>Profile</div>
 		</div>
 	);
 }
