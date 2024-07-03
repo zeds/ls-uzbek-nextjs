cd47b95c4d56e1f9edd4cc419ba538d38ff277b4
zeds <tom.zed39@gmail.com>
2024-07-03 22:07:30 +0700

    tooltip

--- tom/youtube-tailwind/components/Tags.tsx
+++ tom/youtube-tailwind/components/Tags.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 
 export default function Tags() {
-	const tagName = [
-		{ name: "すべて" },
-		{ name: "音楽" },
-		{ name: "ライブ" },
-		{ name: "オーディオブック" },
-		{ name: "フィットネス" },
-		{ name: "最近アップロードされた動画" },
-		{ name: "視聴済み" },
-		{ name: "新しい動画の発見" },
-	];
+  const tagName = [
+    { name: "すべて" },
+    { name: "音楽" },
+    { name: "ライブ" },
+    { name: "オーディオブック" },
+    { name: "フィットネス" },
+    { name: "最近アップロードされた動画" },
+    { name: "視聴済み" },
+    { name: "新しい動画の発見" },
+  ];
 
-	return (
-		<div className="tag-container">
-			<div className="tag-list">
-				{tagName.map((item, index) => (
-					<div key={index} className="tag">
-						{item.name}
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="fixed z-10 top-14 h-14 w-full items-center flex sm:pl-0 md:pl-20 lg:pl-60 bg-white">
+      <div className="tag-list flex whitespace-nowrap overflow-x-scroll ">
+        {tagName.map((item, index) => (
+          <div
+            key={index}
+            className="flex mr-[10px] bg-gray-200 font-medium text-[14px] text-#0f0f0f  py-[5px] px-3 rounded-[8px] h-8 leading-[23px] hover:cursor-pointer hover:bg-red-300"
+          >
+            {item.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
