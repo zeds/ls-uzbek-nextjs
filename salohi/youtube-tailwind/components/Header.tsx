4793824a0e93d775dd6eb964a766423e726d1415
saloh2 <salohiddingulboyev7@gmail.com>
2024-07-05 08:40:43 +0500

    改善

--- salohi/youtube-tailwind/components/Header.tsx
+++ salohi/youtube-tailwind/components/Header.tsx
@@ -18,19 +18,22 @@
 				placeholder="検索"
 				className="pl-4 h-[40px] w-full max-w-[536px] rounded-l-full outline outline-gray-300"
 				></input>
-				<button className=" bg-gray-200 hover:bg-gray-300 shrink-0 w-[64px] h-[40px] relative rounded-r-full border-2 border-solid outline outline-gray-300">
+				<button className=" bg-gray-200 hover:bg-gray-300 shrink-0 w-[64px] h-[40px] rounded-r-full border-2 border-solid outline outline-gray-300 relative tooltip">
 				<img
 					className="w-[24px] absolute right-4 bottom-1.5"
 					src="./images/scope.svg"
 					alt=""
 				></img>
+				<span className="tooltip-text">search</span>
 				</button>
-				<button className="shrink-0 ml-[12px] w-[40px] h-[40px] rounded-full bg-gray-200 hover:bg-gray-300 flex items-center justify-center aspect-square">
-				<img
-					className="flex rounded-full w-[24px]"
-					src="./images/mic.svg"
-					alt=""
-				></img>
+				{/* mic */}
+				<button className="bg-gray-200 hover:bg-gray-300 shrink-0 ml-[12px] w-[40px] h-[40px] rounded-full  flex items-center justify-center aspect-square relative tooltip">
+					<img
+						className="flex rounded-full w-[24px]"
+						src="./images/mic.svg"
+						alt=""
+					></img>
+					<span className="tooltip-text">音声で検索</span>
 				</button>
 			</div>
 			<ul className="flex items-center justify-end w-[225px] h-[40px] shrink-0 gap-2 cursor-pointer">
