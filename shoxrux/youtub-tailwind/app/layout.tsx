e13be39ff584a43f69cdcb5ccd648179af1692df
Shoxrux71 <amirjonovdamin@gmail.com>
2024-07-05 16:39:51 +0500

    article from superbase

--- shoxrux/youtub-tailwind/app/layout.tsx
+++ shoxrux/youtub-tailwind/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<div className="w-full h-dvh bg-sky-300">
+				<div className="w-full h-dvh">
 					<Header />
 					
 					<Tags />
