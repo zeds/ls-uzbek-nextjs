08b1611be637eea437ea36d0e9e79e58d2b6f1b8
Shoxrux71 <amirjonovdamin@gmail.com>
2024-07-10 18:58:21 +0500

    article

--- shoxrux/youtub-tailwind/app/articles/page.tsx
+++ shoxrux/youtub-tailwind/app/articles/page.tsx
@@ -1,55 +1,8 @@
 "use client";
-import React, { useCallback, useEffect, useState } from "react";
-import { createClient } from "@/utils/supabase/client";
 import Articles from "@/components/Articles";
 
 const Page = () => {
-	const [dataSource, setDataSource] = useState([]);
-
-	const supabase = createClient();
-
-	const getArticles = useCallback(async () => {
-		try {
-			const { data, error, status } = await supabase
-				.from("articles")
-				.select("*")
-				.order("id", { ascending: false });
-
-			if (error && status !== 406) {
-				throw error;
-			}
-
-			if (data) {
-				console.log("data = ", data);
-				setDataSource(data);
-			}
-		} catch (error) {
-			alert("Error loading user data!");
-		} finally {
-			//   setLoading(false)
-		}
-	}, []);
-
-	useEffect(() => {
-		getArticles();
-	}, []);
-
-	return (
-		<div>
-			{dataSource.map((item, index) => (
-				<Articles
-					key={index}
-					img={item.img_url}
-					avatar_url={item.avatar_url}
-					title={item.title}
-					user={item.user_name}
-					stats={item.stats}
-				/>
-			))}
-		</div>
-	);
-
-	return <div className="mt-40">{JSON.stringify(dataSource)}</div>;
+	return <Articles />;
 };
 
 export default Page;
--- shoxrux/youtub-tailwind/next.config.mjs
+++ shoxrux/youtub-tailwind/next.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-       remotePatterns: [
-          {
-             protocol: "https",
-             hostname: "i.ytimg.com",
-             pathname: "**",
-          },
-          {
-             protocol: "https",
-             hostname: "yt3.ggpht.com",
-             pathname: "**",
-          },
-       ],
-    },
- };
- 
- export default nextConfig;
+	reactStrictMode: false,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "i.ytimg.com",
+				pathname: "**",
+			},
+			{
+				protocol: "https",
+				hostname: "yt3.ggpht.com",
+				pathname: "**",
+			},
+		],
+	},
+};
+
+export default nextConfig;
