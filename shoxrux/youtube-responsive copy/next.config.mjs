65c8ab4dc0a0be78e55e50a77e1eb601bff56ef7
Shoxrux71 <amirjonovdamin@gmail.com>
2024-07-19 18:03:02 +0500

    add

---
+++ shoxrux/youtube-responsive copy/next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.ggpht.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
