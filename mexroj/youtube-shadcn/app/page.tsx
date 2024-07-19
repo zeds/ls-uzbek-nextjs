5bf7fd4de6bb519ed6f75881af5ac50da61d50ff
mexroj2603 <sayfullayevmexroj27@gmail.com>
2024-07-19 19:21:31 +0500

    000

--- mexroj/youtube-shadcn/app/page.tsx
+++ mexroj/youtube-shadcn/app/page.tsx
@@ -1,18 +1,6 @@
 "use client";
-import Image from "next/image";
-import React, { useCallback, useEffect, useState } from "react";
-import { createClient } from "@/utils/supabase/client";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
-// import "./global.css";
-
-type articleProp = {
-  img_url: string;
-  avatar_url: string;
-  title: string;
-  user_name: string;
-  stats: string;
-};
+import Image from "next/image";
 
 const list = [
   "すべて",
@@ -25,128 +13,85 @@ const list = [
   "新しい動画の発見",
 ];
 
-const navItems = [
-  { title: "ホーム", img: "./images/yt-home.svg" },
-  { title: "ショート", img: "./images/yt-short.svg" },
-  { title: "登録チャンネル", img: "./images/yt-r-channel.svg" },
-  { title: "YouTube Music", img: "./images/yt-r-channel.svg" },
-  { title: "", img: "" },
-  { title: "チャンネル", img: "./images/yt-channel.svg" },
-  { title: "履歴", img: "./images/yt-history.svg" },
-  { title: "再生リスト", img: "./images/yt-play-list.svg" },
-  { title: "再生した動画", img: "./images/yt-my-movie.svg" },
-];
-
 export default function Home() {
-  const [dataSource, setDataSource] = useState<articleProp[]>([]);
-
-  const supabase = createClient();
-
-  const getArticles = useCallback(async () => {
-    try {
-      const { data, error, status } = await supabase
-        .from("articles")
-        .select("*")
-        .order("id", { ascending: false });
-
-      if (error && status !== 406) {
-        throw error;
-      }
-
-      if (data) {
-        console.log("data = ", data);
-        setDataSource(data);
-      }
-    } catch (error) {
-      alert("Error loading user data!");
-    } finally {
-      //   setLoading(false)
-    }
-  }, []);
-
-  useEffect(() => {
-    getArticles();
-  }, []);
-
   return (
-    <div className="w-full h-full flex pt-14 hidden-scrollbar">
-      {/* navbar */}
-      <nav className="hidden md:flex md:w-[72px] md:py-0 md:px-1  md:flex-col fixed lg:w-[240px] left-0 top-14 bottom-0 z-20 bg-white  ">
-        <ul className="pl-[10px] ">
-          {navItems.map((item, index) => (
-            <>
-              {item.title === "" ? (
-                <div className="border-t-[1px] border-solid border-gray-200 mt-3 pt-3" />
-              ) : (
-                <li
-                  key={index}
-                  className="flex lg:text-[14px] font-medium lg:leading-4 text-[rgba(15,15,15,1)] lg:p-[10px]  items-center rounded-[10px] lg:gap-6 hover:bg-gray-200 md:flex-col md:justify-center  md:gap-0 md:text-[10px] md:leading-[14px] md:rounded-[10px] md:p-1  lg:flex-row lg:justify-start"
-                >
-                  <img src={item.img} alt="home" className="w-6 h-6 mb-[6px]" />
-                  <span className="title ">{item.title} </span>
-                </li>
-              )}
-            </>
-          ))}
-        </ul>
-      </nav>
-
-      {/* main */}
-      <div>
-        {/* tags */}
-        <ul className="fixed z-10 top-14 h-14 w-full items-center flex sm:pl-0 md:pl-20 lg:pl-60 overflow-y-scroll hidden-scrollbar bg-white">
-          {list.map((item, index) => (
-            <li key={index}>
-              <Button>{item}</Button>
+    <div className="w-full h-dvh flex pt-14 pr-5">
+      <div className="fixed z-20 h-dvh hidden md:block md:w-[80px] xl:w-[270px] bg-pink-300">
+        <nav>
+          <ul>
+            <li className="flex px-[12px] items-center  bg-red-500 py-3 text-[14px] h-[40px] gap-6">
+              <img src="images/yt-home.svg" alt="home" />
+              <div>ホーム</div>
             </li>
-          ))}
-        </ul>
+          </ul>
+        </nav>
+      </div>
 
-        {/* articles */}
-        <div className="grid gap-3  px-4 md:pl-20 lg:pl-60 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 hidden-scrollbar overflow-y-scroll">
-          {dataSource.map((item, index) => (
-            <div key={index}>
-              <Link href={"./videoplay"}>
-                <div className="relative ">
-                  <Image
-                    width={0}
-                    height={0}
-                    src={item.img_url}
-                    alt="hero"
-                    sizes="100vw"
-                    style={{ width: "100%", height: "auto" }}
-                  />
-                  <span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
-                    10:00
-                  </span>
-                </div>
-              </Link>
+      {/* tag */}
+      <ul className="fixed w-full flex  pl-5 md:pl-[90px] xl:pl-[280px] gap-3 p-1 overflow-y-scroll hidden-scrollbar">
+        {list.map((item, index) => (
+          <li key={index}>
+            <Button>{item}</Button>
+          </li>
+        ))}
+      </ul>
 
-              <div className="flex gap-[10px] mt-3 mr-2 p-1">
-                <Image
-                  width={0}
-                  height={0}
-                  src={item.avatar_url}
-                  alt="avatar"
-                  className="w-9 h-9 rounded-full"
-                />
-                <div>
-                  <div className="line-clamp-2 leading-[22px] text-[rgba(15,15,15,1)]">
-                    {item.title}
-                    {/* Free BGM "I'll be sleepy after a snack" 2 hours ver -
-                            Kawaii Afternoon Break [NoCopyrightMusic] */}
-                  </div>
-                  <div className="line-clamp-1 text-sm font-normal text-[rgba(15,15,15,1)]">
-                    {item.user_name}
-                  </div>
-                  <div className="line-clamp-1 text-sm font-normal text-[rgba(96,96,96,1)]">
-                    {item.stats}
-                  </div>
-                </div>
-              </div>
+      {/* articles */}
+      <div className=" pt-[70px] h-fit w-full grid grid-cols-1 pl-5 md:pl-[90px] xl:pl-[280px] sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-3">
+        <div className="bg-red-300">
+          <div className="relative">
+            <img src="diana.webp" alt="diana" />
+            <span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
+              10:00
+            </span>
+          </div>
+          <div className="flex">
+            <img className="w-9 h-9 rounded-full" src="tom.jpeg" alt="tom" />
+            <div>
+              <div>title</div>
+              <div>user</div>
+              <div>stats</div>
             </div>
-          ))}
+          </div>
         </div>
+        <div className="bg-red-300">
+          <div className="relative">
+            <img src="diana.webp" alt="diana" />
+            <span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
+              10:00
+            </span>
+          </div>
+          <div className="flex">
+            <img className="w-9 h-9 rounded-full" src="tom.jpeg" alt="tom" />
+            <div>
+              <div>title</div>
+              <div>user</div>
+              <div>stats</div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-red-300">
+          <div className="relative">
+            <img src="diana.webp" alt="diana" />
+            <span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
+              10:00
+            </span>
+          </div>
+          <div className="flex">
+            <img className="w-9 h-9 rounded-full" src="tom.jpeg" alt="tom" />
+            <div>
+              <div>title</div>
+              <div>user</div>
+              <div>stats</div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-red-300">item-2</div>
+        <div className="bg-red-300">item-3</div>
+        <div className="bg-red-300">item-4</div>
+        <div className="bg-red-300">item-5</div>
+        <div className="bg-red-300">item-6</div>
+        <div className="bg-red-300">item-7</div>
       </div>
     </div>
   );
--- mexroj/youtube-shadcn/next.config.mjs
+++ mexroj/youtube-shadcn/next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.ytimg.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "yt3.ggpht.com",
-        pathname: "**",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
