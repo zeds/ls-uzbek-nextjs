7009611c54058698a05fbc3051dc960f59939b62
Ziynat555 <ziynat.zl56@gmail.com>
2024-07-22 15:33:37 +0500

    ziynat

--- ziynat/youtube-shadcn/app/login/page.tsx
+++ ziynat/youtube-shadcn/app/login/page.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import React, { useState } from "react";
 
-const page = () => {
-  return <div className="bg-red-300">page</div>;
+const Page = () => {
+  const [message, setMessage] = useState("6文字以上入力する必要があります");
+  return (
+    <div className="flex w-full h-dvh bg-red-300 justify-center">
+      <div className="w-[600px] h-fit bg-green-400 p-5">
+        <div className="my-5 text-center">ログイン</div>
+        <br />
+        <Input className="my-5"></Input>
+        <Input></Input>
+        <div className="my-2 text-center">{message}</div>
+        <Button className="w-full">ログイン</Button>
+        <br />
+        <div className="my-2 w-full justify-center flex">
+          <Link href="/forgot-password">パスワードを忘れた方はこちら</Link>
+        </div>
+        <div className="my-2 w-full justify-center flex">
+          <Link href="/signup">アカウントを作成する</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
