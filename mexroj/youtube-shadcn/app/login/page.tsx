f467db37fe2328dd58e66c59ca02f81a4bbcddd7
mexroj2603 <sayfullayevmexroj27@gmail.com>
2024-07-24 17:57:55 +0500

    login

--- mexroj/youtube-shadcn/app/login/page.tsx
+++ mexroj/youtube-shadcn/app/login/page.tsx
@@ -1,24 +1,86 @@
 "use client";
+import React, { useCallback, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
-import { Router } from "next/router";
-import React, { useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+import { useCounterStore } from "@/store";
 
 const Page = () => {
-  const [message, setMessage] = useState("6文字以上入力する必要があります");
-  return (
-    <div className="pt-[56px] w-full h-dvh bg-red-300 justify-center items-center flex">
-      <div className="flex flex-col w-[600px] h-fit bg-gray-200 p-4 rounded-sm">
-        <div className="text-lg font-bold flex justify-center">ログイン</div>
-        <Input className="my-5" type="email" placeholder="メールアドレス" />
-        <Input className="my-5" type="password" placeholder="パスワード" />
-        <div className="text-red-500">{message}</div>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        <Button className="mt-5 w-full">ログイン</Button>
-        <div className="flex justify-between items-center my-5">
-          <Link href={"/"}>パスワードを忘れた</Link>
-          <Link href={"/"}>新規登録はこちら</Link>
+  const [message, setMessage] = useState("6文字以上入力する必要があります");
+  const supabase = createClient();
+  const { setLogin, setUser, user } = useCounterStore();
+
+  const route = useRouter();
+
+  const clickLogin = async () => {
+    let { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+    if (error) {
+      setMessage("エラーです。");
+    } else {
+      // profilesデータを取得
+      if (data) {
+        getProfile(data.user?.id);
+      }
+    }
+  };
+
+  const getProfile = useCallback(async (profileId: any) => {
+    try {
+      const { data, error, status } = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("id", profileId)
+        .single();
+
+      if (error) {
+        throw error;
+      }
+
+      setMessage("ログイン成功");
+      setUser(data);
+      setLogin(true);
+      route.push("/");
+    } catch (error) {
+      alert("Error loading user data!");
+    } finally {
+      //   setLoading(false)
+    }
+  }, []);
+
+  return (
+    <div className="flex w-full h-dvh bg-red-200 justify-center items-center">
+      <div className="w-[600px] h-fit bg-green-200 p-5 mt-5">
+        <div className="my-5 text-center">ログイン</div>
+        <Input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="メールアドレス"
+          className="mb-4"
+        />
+        <Input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="パスワード"
+        />
+        <div className="my-2 text-center">{message}</div>
+        <Button onClick={() => clickLogin()} className="w-full mt-5">
+          ログイン
+        </Button>
+        <div className="my-2 w-full justify-center flex">
+          <Link href="/forgot-password">パスワードを忘れた方はこちら</Link>
+        </div>
+        <div className="my-2w-full justify-center flex">
+          <Link href="/signup">アカウントを作成する</Link>
         </div>
       </div>
     </div>
