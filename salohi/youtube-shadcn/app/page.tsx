447e7adcfac67f8c7c67116be1db55e34f0def2b
saloh2 <salohiddingulboyev7@gmail.com>
2024-07-24 19:18:37 +0500

    kaizen

--- salohi/youtube-shadcn/app/page.tsx
+++ salohi/youtube-shadcn/app/page.tsx
@@ -1,246 +1,131 @@
-	import { Button } from "@/components/ui/button";
-	import {
-		Cloud,
-		CreditCard,
-		Github,
-		Keyboard,
-		LifeBuoy,
-		LogOut,
-		Mail,
-		MessageSquare,
-		Plus,
-		PlusCircle,
-		Settings,
-		User,
-		UserPlus,
-		Users,
-	} from "lucide-react";
-	import {
-		DropdownMenu,
-		DropdownMenuContent,
-		DropdownMenuGroup,
-		DropdownMenuItem,
-		DropdownMenuLabel,
-		DropdownMenuPortal,
-		DropdownMenuSeparator,
-		DropdownMenuShortcut,
-		DropdownMenuSub,
-		DropdownMenuSubContent,
-		DropdownMenuSubTrigger,
-		DropdownMenuTrigger,
-	} from "@/components/ui/dropdown-menu";
-	import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
-	export default function Home() {
-	const tagName = [
-		{ name: "すべて" },
-		{ name: "音楽" },
-		{ name: "ライブ" },
-		{ name: "オーディオブック" },
-		{ name: "フィットネス" },
-		{ name: "最近アップロードされた動画" },
-		{ name: "視聴済み" },
-		{ name: "新しい動画の発見" },
-	];
+
+const list = [
+	"すべて",
+	"ライブ",
+	"オーディオブック",
+	"観光",
+	"フィットネス",
+	"最近アップロードされた動画",
+	"視聴済み",
+	"新しい動画の発見",
+];
+const items = [
+	{ title: "ホーム", img: "./images/yt-home.svg" },
+	{ title: "ショート", img: "./images/yt-short.svg" },
+	{ title: "登録チャンネル", img: "./images/yt-r-channel.svg" },
+	{ title: "YouTube Music", img: "./images/yt-r-channel.svg" },
+	{ title: "", img: "" },
+	{ title: "チャンネル", img: "./images/yt-channel.svg" },
+	{ title: "履歴", img: "./images/yt-history.svg" },
+	{ title: "再生リスト", img: "./images/yt-play-list.svg" },
+	{ title: "再生した動画", img: "./images/yt-my-movie.svg" },
+];
+
+export default function Home() {
 	return (
-		<div className="w-full h-dvh bg-sky-300 flex">
-		<ul className="fixed z-10 top-14 w-full items-center pl-2  md:pl-[90px] lx:pl-[270] bg-white  overflow-scroll tag-list">
-			<li className="flex">
-			{tagName.map((item, index) => (
+		<div className="w-full h-dvh flex pt-14 pr-5">
+			<nav className="fixed z-20 h-dvh hidden md:block md:w-[80px] lg:w-[270px] bg-white">
+		<ul className="pl-[10px]">
+			{items.map((item, index) => (
+			<>
+				{item.title === "" ? (
+				<div className="border-t-[1px] border-solid border-gray-200 mt-3 pt-3" />
+				) : (
 				<li
-				key={index}
-				className="flex mr-[10px] bg-gray-200 font-medium text-[14px] text-#0f0f0f  py-[5px] px-3 rounded-[8px] h-8 leading-[23px] hover:cursor-pointer hover:bg-slate-300 whitespace-nowrap"
+					key={index}
+					className="flex lg:text-[16px] font-medium lg:leading-4 text-(rgba[15,15,15,1]) lg:p-[10px]  items-center rounded-[10px] lg:gap-6 hover:bg-gray-200 md:flex-col md:justify-center  md:gap-0 md:text-[10px] md:leading-[14px] md:rounded-[10px] md:p-1  lg:flex-row lg:justify-start"
 				>
-				{item.name}
+					<img src={item.img} alt="home" className="w-6 h-6 mb-[6px]" />
+					<span className="title ">{item.title} </span>
 				</li>
+				)}
+			</>
 			))}
-			</li>
 		</ul>
-		<div className="fixed z-20 h-dvh hidden md:block md:w-[80px] lg:w-[270px] bg-red-300">
-			nav
-		</div>
-		<div className="pt-[56px] pl-3 pr-3 bg-white h-fit w-full grid grid-cols-1 md:pl-[80px] xl:pl-[270px] sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-3">
-			<div className="bg-red-300">
-			<div className="relative ">
-				<img src="./images/raichel.webp" alt="diana" />
-				<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
-				10:00
-				</span>
-			</div>
-			<div className="flex gap-2">
-				<img
-				className="w-9 h-9 rounded-full pl-1 pt-1"
-				src="./images/my foto.jpg"
-				alt="saloh"
-				/>
-				<div>
-				<div>title</div>
-				<div>user</div>
-				<div>stats</div>
+		</nav>
+
+			{/* tag */}
+			<ul className="fixed w-full flex  pl-5 md:pl-[90px] lg:pl-[280px] gap-3 p-1 overflow-y-scroll hidden-scrollbar">
+				{list.map((item, index) => (
+					<li key={index}>
+					<Button>{item}</Button>
+					</li>
+				))}
+			</ul>
+
+			{/* articles */}
+			<div className=" pt-[70px] h-fit w-full grid grid-cols-1 pl-5 md:pl-[90px] lg:pl-[280px] sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-3">
+				<div className="bg-red-300">
+					<div className="relative">
+						<img src="diana.webp" alt="diana" />
+						<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
+							10:00
+						</span>
+					</div>
+					<div className="flex">
+						<img
+							className="w-9 h-9 rounded-full"
+							src="tom.jpeg"
+							alt="tom"
+						/>
+						<div>
+							<div>title</div>
+							<div>user</div>
+							<div>stats</div>
+						</div>
+					</div>
 				</div>
-			</div>
-			</div>
-			<div className="bg-red-300">
-			<div className="relative">
-				<img src="./images/raichel.webp" alt="diana" />
-				<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
-				10:00
-				</span>
-			</div>
-			<div className="flex gap-2">
-				<img
-				className="w-9 h-9 rounded-full pl-1 pt-1"
-				src="./images/my foto.jpg"
-				alt="saloh"
-				/>
-				<div>
-				<div>title</div>
-				<div>user</div>
-				<div>stats</div>
+				<div className="bg-red-300">
+					<div className="relative">
+						<img src="diana.webp" alt="diana" />
+						<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
+							10:00
+						</span>
+					</div>
+					<div className="flex">
+						<img
+							className="w-9 h-9 rounded-full"
+							src="tom.jpeg"
+							alt="tom"
+						/>
+						<div>
+							<div>title</div>
+							<div>user</div>
+							<div>stats</div>
+						</div>
+					</div>
 				</div>
-			</div>
-			</div>
-			<div className="bg-red-300">
-			<div className="relative">
-				<img src="./images/raichel.webp" alt="diana" />
-				<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
-				10:00
-				</span>
-			</div>
-			<div className="flex gap-2">
-				<img
-				className="w-9 h-9 rounded-full pl-1 pt-1"
-				src="./images/my foto.jpg"
-				alt="saloh"
-				/>
-				<div>
-				<div>title</div>
-				<div>user</div>
-				<div>stats</div>
+				<div className="bg-red-300">
+					<div className="relative">
+						<img src="diana.webp" alt="diana" />
+						<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
+							10:00
+						</span>
+					</div>
+					<div className="flex">
+						<img
+							className="w-9 h-9 rounded-full"
+							src="tom.jpeg"
+							alt="tom"
+						/>
+						<div>
+							<div>title</div>
+							<div>user</div>
+							<div>stats</div>
+						</div>
+					</div>
 				</div>
+				<div className="bg-red-300">item-2</div>
+				<div className="bg-red-300">item-3</div>
+				<div className="bg-red-300">item-4</div>
+				<div className="bg-red-300">item-5</div>
+				<div className="bg-red-300">item-6</div>
+				<div className="bg-red-300">item-7</div>
 			</div>
-			</div>
-			<div className="bg-red-300">
-			<div className="relative">
-				<img src="./images/raichel.webp" alt="diana" />
-				<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
-				10:00
-				</span>
-			</div>
-			<div className="flex gap-2">
-				<img
-				className="w-9 h-9 rounded-full pl-1 pt-1"
-				src="./images/my foto.jpg"
-				alt="saloh"
-				/>
-				<div>
-				<div>title</div>
-				<div>user</div>
-				<div>stats</div>
-				</div>
-			</div>
-			</div>
-			<div className="bg-red-300">
-			<div className="relative">
-				<img src="./images/raichel.webp" alt="diana" />
-				<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
-				10:00
-				</span>
-			</div>
-			<div className="flex gap-2">
-				<img
-				className="w-9 h-9 rounded-full pl-1 pt-1"
-				src="./images/my foto.jpg"
-				alt="saloh"
-				/>
-				<div>
-				<div>title</div>
-				<div>user</div>
-				<div>stats</div>
-				</div>
-			</div>
-			</div>
-			<div className="bg-red-300">
-			<div className="relative">
-				<img src="./images/raichel.webp" alt="diana" />
-				<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
-				10:00
-				</span>
-			</div>
-			<div className="flex gap-2">
-				<img
-				className="w-9 h-9 rounded-full pl-1 pt-1"
-				src="./images/my foto.jpg"
-				alt="saloh"
-				/>
-				<div>
-				<div>title</div>
-				<div>user</div>
-				<div>stats</div>
-				</div>
-			</div>
-			</div>
-			<div className="bg-red-300">
-			<div className="relative">
-				<img src="./images/raichel.webp" alt="diana" />
-				<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
-				10:00
-				</span>
-			</div>
-			<div className="flex gap-2">
-				<img
-				className="w-9 h-9 rounded-full pl-1 pt-1"
-				src="./images/my foto.jpg"
-				alt="saloh"
-				/>
-				<div>
-				<div>title</div>
-				<div>user</div>
-				<div>stats</div>
-				</div>
-			</div>
-			</div>
-			<div className="bg-red-300">
-			<div className="relative">
-				<img src="./images/raichel.webp" alt="diana" />
-				<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
-				10:00
-				</span>
-			</div>
-			<div className="flex gap-2">
-				<img
-				className="w-9 h-9 rounded-full pl-1 pt-1"
-				src="./images/my foto.jpg"
-				alt="saloh"
-				/>
-				<div>
-				<div>title</div>
-				<div>user</div>
-				<div>stats</div>
-				</div>
-			</div>
-			</div>
-			<div className="bg-red-300">
-			<div className="relative">
-				<img src="./images/raichel.webp" alt="diana" />
-				<span className="absolute bottom-0 right-0 bg-[rgba(0,0,0,0.7)] text-white p-1">
-				10:00
-				</span>
-			</div>
-			<div className="flex gap-2">
-				<img
-				className="w-9 h-9 rounded-full pl-1 pt-1"
-				src="./images/my foto.jpg"
-				alt="saloh"
-				/>
-				<div>
-				<div>title</div>
-				<div>user</div>
-				<div>stats</div>
-				</div>
-			</div>
-			</div>
-		</div>
 		</div>
 	);
-	}
+}
